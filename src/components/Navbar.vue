c8d6ad97459c876728c592086db760fca76ba46a
jhorevellegamilla <jhorevellegamilla@gmail.com>
2025-07-29 11:06:50 +0800

    fix img

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 	  <div class="container" id="nav">
 	  	<a class="navbar-brand" href="#">Jhorevelle</a>
 	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
